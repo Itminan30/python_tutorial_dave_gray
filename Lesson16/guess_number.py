f0ff63c249569484b482afd3f06a974633e7a53c
Itminan30 <itminan30@gmail.com>
2024-12-21 12:01:51 +0600

    lesson 16 started

---
+++ Lesson16/guess_number.py
@@ -0,0 +1,18 @@
+########################################################################################################################
+# {name}, guess which number I'm thinking of ... 1, 2 or 3.
+# {name}, you chose {number}.
+# I was thinking about number {number}
+# 🎉 {name}, you win! 🎉 || 😔 Sorry, {name}. Better luck next time. 😔
+# Game count: {number of total games}
+# {name}'s wins: {number of wins by the player}
+# Your winning percentage: {wining percentage}%
+# Play again, {name}?
+# Y for Yes or Q for Quit
+# {name}, please enter 1, 2 or 3.
+# 🎊🎊 Thank you for playing 🎊🎊
+# 👋👋 Bye, {name} 👋👋
+########################################################################################################################
+
+import sys
+from random import choice
+
